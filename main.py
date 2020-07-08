2e417508611e56a2d08a15fae11f9d1638707c57
antipups <nikitakurkurin@protonmail.ch>
2020-07-08 15:58:43 +0300

    Подключена бдшка, сделан парс нескольких фотографий и отправка их с подписью.

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-import telebot
-import requests
-import re
-import config
-import pprint
+import time
 
+import telebot
+import config
+import vk
 
 bot = telebot.TeleBot(token=config.tg_token)
 
 
-@bot.message_handler(commands=['start',])
-def get_id(message):
-    print(message)
+# @bot.message_handler(commands=['start',])
+# def get_id(message):
+#     print(message)
 
 
-# def parse():
-#     bot.send_message()
+def parse():
+    while True:
+        result = vk.parse_group()
+        if result.get('attachments'):
+            bot.send_media_group(chat_id=config.my_chat_id, media=(telebot.types.InputMediaPhoto(result.get('attachments')[0], caption=result.get('text')),
+                                                                   *(telebot.types.InputMediaPhoto(media) for media in result.get('attachments')[1:])))
+        else:
+            bot.send_message(chat_id=config.my_chat_id, text=result.get('text'))
+        time.sleep(60)
 
 
 if __name__ == '__main__':
-    # parse()
-    bot.infinity_polling()
+    parse()
+    # bot.infinity_polling()
--- vk.py
+++ vk.py
@@ -1,20 +1,36 @@
 import config
 import requests
+import sqlite3
 
 
-def parse_group(id_group):
-    html_code = requests.get(f'https://api.vk.com/method/wall.get?domain={id_group}&count=2&access_token={config.vk_token}&v=5.120').text
-    dict_of_code = eval(html_code.replace('false', 'False').replace('true', 'True').replace('null', 'None'))
-    post = dict_of_code.get('response').get('items')[-1]
-    result_dict = {}
-    result_dict['text'] = post.get('text')
-    attachments = post.get('attachments')
-    result_dict['attachments'] = []
-    for attachment in attachments:
-        if attachment.get('photo'):
-            result_dict.get('attachments').append(max(attachment.get('photo').get('sizes'), key=lambda x: x.get('height')).get('url').replace('\\', ''))
-    return result_dict
+def parse_group():
+    """
+        Парсим вк группы, берем их из БД, и после парсим
+    :return:
+    """
+
+    connect = sqlite3.connect(database=config.name_of_db)
+    cursor = connect.cursor()
+
+    groups = (row[0] for row in cursor.execute('SELECT title_group FROM groups'))
+
+    list_comeback = []
+    for group in groups:
+        html_code = requests.get(f'https://api.vk.com/method/wall.get?domain={group}&count=5&access_token={config.vk_token}&v=5.120').text
+        dict_of_code = eval(html_code.replace('false', 'False').replace('true', 'True').replace('null', 'None'))
+        post = dict_of_code.get('response').get('items')[-1]
+
+        result_dict = {}
+        result_dict['text'] = post.get('text')
+        attachments = post.get('attachments')
+        result_dict['attachments'] = []
+        for attachment in attachments:
+            if attachment.get('photo'):
+                result_dict.get('attachments').append(requests.get(max(attachment.get('photo').get('sizes'), key=lambda x: x.get('height')).get('url').replace('\\', '')).content)
+        list_comeback.append(result_dict)
+
+    return list_comeback
 
 
 if __name__ == '__main__':
-    print(parse_group('peregovorov'))
+    print(parse_group())

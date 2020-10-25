a8f81dd29751f292d64af82b454c844f56b8a154
antipups <51057725+antipups@users.noreply.github.com>
2020-10-26 00:41:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ tg_token = ''
 vk_token = ''
 name_of_db = 'tg_bot_vk_parse.db'
 # 704369002
-# my_chat_id = 352205080
-my_chat_id = 704369002
-channel_id = 704369002
+# my_chat_id = 
+my_chat_id = 
+channel_id = 
 
 
 dict_of_data_about_post = []
